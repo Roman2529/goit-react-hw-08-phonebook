337956b1cb6c7b717de53dd949eefd5b569387de
Roman Demikhov <demikhovroman@gmail.com>
2020-09-29 22:10:17 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-08-phonebook/precache-manifest.c1d427fc1eecd786be4ab5032d52618e.js"
+  "/goit-react-hw-08-phonebook/precache-manifest.ae5d364c599414e0cc30b378f533ba65.js"
 );
 
 self.addEventListener('message', (event) => {
